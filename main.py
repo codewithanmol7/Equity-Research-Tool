7810c61918d72046dabdf10fdc2d28936834de0e
codewithanmol7 <139151456+codewithanmol7@users.noreply.github.com>
2026-02-22 10:35:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pickle
 import time
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_community.chains import RetrievalQAWithSourcesChain
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import FAISS
